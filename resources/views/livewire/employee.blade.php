38f3c7f68723e52df1458af713df96c13ff7dcdb
dale0333 <alvindalejoyosa30@gmail.com>
2026-09-11 10:24:18 +0800

    fix label

--- resources/views/livewire/employee.blade.php
+++ resources/views/livewire/employee.blade.php
@@ -3,10 +3,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mx-auto mb-8 max-w-2xl text-center">
                 <h1 class="text-2xl font-semibold text-gray-900">
-                    Employee updated
+                    Employee
                 </h1>
                 <p class="mt-2 text-sm text-gray-600">
-                    Create and manage employee records totoo
+                    Create and manage employee
                 </p>
             </div>
 
